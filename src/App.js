d2cb0e8738eff6cf5b60b621772c2f1306e7d2bf
BrownEdgar <nikogosjanedgar@gmail.com>
2021-11-12 22:15:28 +0400

    add first version

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+// import MuiTypography from './Components/Typography/MuiTypography';
+// import WithCss from './Components/makeCss/WithCss';
+// import Buttons from './Components/Buttons/Buttons';
+import Breakpoints from './Components/Breakpoints/Breakpoints';
+
+
+function App() {
+	return (
+		<div>
+			{/* <MuiTypography/> */}
+			{/* <WithCss/> */}
+			{/* <Buttons/> */}
+			<Breakpoints/>
+		</div>
+	)
+}
+
+export default App
+
